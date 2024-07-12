8b0cd9bd4292a06bfaa804c6baa6ea33f332c62a
Virinchi-5818 <161412713+Virinchi-5818@users.noreply.github.com>
2024-07-12 13:17:26 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,15 @@
+class Pyramid
+{
+public static void main(String args[])
+{
+int n1=Integer.parseInt(args[0]);
+for(int i=0;i<=n1;i++)
+{
+	for(int j=0;j<i;j++)
+	{
+		System.out.print(i+'\t');
+		}
+	System.out.println();
+	}
+	}
+	}
