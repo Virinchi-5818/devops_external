cbab7cef6688a487fb4f6eef7e0e307939b7f3e4
Virinchi-5818 <161412713+Virinchi-5818@users.noreply.github.com>
2024-07-12 13:20:54 +0530

    Update Pyramid.java

--- Pyramid.java
+++ Pyramid.java
@@ -3,13 +3,13 @@ class Pyramid
 public static void main(String args[])
 {
 int n1=Integer.parseInt(args[0]);
-for(int i=0;i<=n1;i++)
+for (int i = 1; i <= n1; ++i) 
 {
-	for(int j=0;j<i;j++)
-	{
-		System.out.print(i+'\t');
-		}
-	System.out.println();
+      for (int j = 1; j <= i; ++j) 
+      {
+        System.out.print(i+" ");
+      }
+      System.out.println();
 	}
 	}
 	}
